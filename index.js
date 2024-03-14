84de9c5b12d31a7141709a7371add23e861b249e
Jessica Kiroff <jesskiroff@gmail.com>
2024-03-14 11:32:57 -0400

    use jQuery to manipulate CSS in the index.js file

---
+++ index.js
@@ -0,0 +1,8 @@
+$("h1").css("color", 'Blue');
+
+//console.log($("h1").css("color")); //Will give an output of blue in the console
+
+//The code below is the same as saying document.querySelectorAll("button").css("color", "yellow");
+//The dollar sign ($) stands for jQuery
+
+$("button").css("color", "pink");
