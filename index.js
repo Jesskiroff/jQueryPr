38857b7783fe5cee724081d4b8f096d2e13e9418
Jessica Kiroff <jesskiroff@gmail.com>
2024-03-14 11:46:09 -0400

    work on CSS via creating classes

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-$("h1").css("color", 'Blue');
+$("h1").addClass("big-title margin-50");
+$("button")
 
-//console.log($("h1").css("color")); //Will give an output of blue in the console
-
-//The code below is the same as saying document.querySelectorAll("button").css("color", "yellow");
-//The dollar sign ($) stands for jQuery
-
-$("button").css("color", "pink");
+//Just as you can add a class, so can you remove a class
+//$("h1").removeClass("big-title"); //This would remove the class of big title, which would essentially remove all of the CSS that 
+$("h1").hasClass("margin-50"); //This will output a boolean of true in the console bc my h1 element does, in fact, have a class of margin-50
